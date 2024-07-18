cc1f87b9c3c516aabc215b992814f040694d9bff
Oliver Smith <olie.smit0@gmail.com>
2024-07-18 13:23:07 +0100

    Reserve all space upfront to avoid extra allocations at load.

--- Source/Core/LowerLevelMeshBVH.cpp
+++ Source/Core/LowerLevelMeshBVH.cpp
@@ -14,6 +14,7 @@ namespace Core
             m_name = mesh->mName.C_Str();
 
             // Copy the index data
+            mIndicies.reserve(mesh->mNumFaces * 3);
             for(uint32_t i = 0; i < mesh->mNumFaces; ++i)
             {
                 for(uint32_t j = 0; j < mesh->mFaces[i].mNumIndices; ++j)
@@ -45,6 +46,7 @@ namespace Core
             }
 
             std::vector<BVH<uint32_t>::BoundedValue> primitive_bounds;
+            primitive_bounds.reserve(mIndicies.size());
             for(uint32_t i = 0; i < mIndicies.size(); i += 3)
             {
                 glm::vec3 min = glm::vec3(INFINITY, INFINITY, INFINITY);
@@ -82,6 +84,7 @@ namespace Core
 
         void LowerLevelMeshBVH::generate_sampling_data()
         {
+            m_triangle_faces.reserve(mIndicies.size() / 3);
             for(uint32_t i_index = 0; i_index < mIndicies.size(); i_index += 3)
             {
                 TriangleFace face{};
@@ -163,19 +166,19 @@ namespace Core
             const glm::vec3& firstPosition = m_positions[firstIndex];
             const glm::vec2& firstuv = !m_uvs ? glm::vec2(0.0f) : m_uvs[firstIndex];
             const glm::vec3& firstNormal = m_normals[firstIndex];
-            const glm::vec4 firstColour = m_colours[firstIndex];
+            const glm::vec4 firstColour = !m_colours ? glm::vec4(0, 0, 0, 0) : m_colours[firstIndex];
 
             const uint32_t secondIndex = m_indicies[baseIndiciesIndex + 1];
             const glm::vec3& secondPosition = m_positions[secondIndex];
             const glm::vec2& seconduv = !m_uvs ? glm::vec2(0.0f) : m_uvs[secondIndex];
             const glm::vec3& secondNormal = m_normals[secondIndex];
-            const glm::vec4& secondColour = m_colours[secondIndex];
+            const glm::vec4& secondColour = !m_colours ? glm::vec4(0, 0, 0, 0) : m_colours[secondIndex];
 
             const uint32_t thirdIndex = m_indicies[baseIndiciesIndex + 2];
             const glm::vec3& thirdPosition = m_positions[thirdIndex];
             const glm::vec2& thirduv = !m_uvs ? glm::vec2(0.0f) : m_uvs[thirdIndex];
             const glm::vec3& thirdNormal = m_normals[thirdIndex];
-            const glm::vec4& thirdColour = m_colours[thirdIndex];
+            const glm::vec4& thirdColour = !m_colours ? glm::vec4(0, 0, 0, 0) : m_colours[thirdIndex];
 
             InterpolatedVertex frag{};
             frag.mPosition = glm::vec4(((1.0f - v - u) * firstPosition) + (u * secondPosition) + (v * thirdPosition), 1.0f);
